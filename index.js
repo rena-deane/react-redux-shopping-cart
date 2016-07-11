2fe8fb40c8f8b4bdefd7048a2686a35f40deb77d
shakhoond <shayan.akhoond@gmail.com>
2016-07-11 15:58:32 +1200

    Updates

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("welcome to shopping-cart")
+import React from 'react'
+import { render } from 'react-dom'
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+
+import reducer from './app/reducer'
+import App from './app/components/app.jsx'
+
+const store = createStore(reducer)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('app')
+)
+
